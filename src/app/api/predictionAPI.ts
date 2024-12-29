2e96c68994c5585ffeae0acf5bd724829476637d
ThatFit3 <m.nurfitri08@gmail.com>
2024-12-30 06:57:40 +0800

    the items use lowercase instead

--- src/app/api/predictionAPI.ts
+++ src/app/api/predictionAPI.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const items:any = require("../../jsons/items.json");
 
 export const predict = async(inputs : Array<string>, location: number) => {
-    let items_cat = inputs.map((input) => items[input]).filter((item) => item !== null && item !== undefined);
+    let items_cat = inputs.map((input) => items[input.toLowerCase()]).filter((item) => item !== null && item !== undefined);
     
     let res = await axios.post("http://127.0.0.1:5000", {
         "category": items_cat,
